c838189cb4974720fc124eb897a1b54e49a24808
Herrmylan <herr.milan.boldi@gmail.com>
2026-07-20 17:49:17 +0200

    phone, tablet size

---
+++ scripts/scroll.js
@@ -0,0 +1,9 @@
+const navbar = document.querySelector(".custom-navbar");
+
+window.addEventListener("scroll", () => {
+    if (window.scrollY > 100) {
+        navbar.classList.add("scrolled");
+    } else {
+        navbar.classList.remove("scrolled");
+    }
+});
